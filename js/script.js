69fdf17fd136d835ca5eccf878fffaba289e9c27
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-27 17:46:19 +0100

    [Fix]

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@
 
 const countdown = document.getElementById('countdown')
 const numRandomList = document.getElementById('numbers-list')
-const numUser = document.querySelector('input')
+const numUser = document.querySelectorAll('#input-group input')
 const button = document.querySelector('button')
 const message = document.getElementById('message')
 const instructions = document.getElementById('instructions')
 const answersForm = document.getElementById('answers-form')
+
 
 
 // definisco il timer
@@ -37,7 +38,7 @@ setTimeout(() => {
 function generateRandomNum(){
     let ArrayNumRandom = [];
     for (let i=0; i< 5; i++){
-        ArrayNumRandom.push(Math.floor(Math.random() * 100) +1)
+        ArrayNumRandom.push(Math.floor(Math.random() * 50) +1)
     }
 
     return ArrayNumRandom
@@ -60,3 +61,6 @@ for (let i=0; i<numRandom.length; i++){
 
 // assegno a numRandomList
 numRandomList.innerHTML = items
+
+
+
