1c9f9fb3bc0b09bdb0d2409b94357d4b74589cb7
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-27 16:00:30 +0100

    [UPD] Gestione timer

--- js/script.js
+++ js/script.js
@@ -7,5 +7,33 @@ const numUser = document.querySelector('input')
 const button = document.querySelector('button')
 const message = document.getElementById('message')
 
+
+// definisco il timer
+let timer;
+
+// gestione del timer
+let count = 0;
+numRandomList.innerText = count;
+
+timer = setInterval(() => {
+    count++;
+    numRandomList.innerText = count;
+},1000)
+
 // Definisco l'array che conterrà i numeri random
 const numRandom = [''];
+
+
+
+// dichiaro l'elemento della lista vuoto
+
+let items = '';
+
+// ciclo l'array
+for (let i=0; i<numRandom.length; i++){
+    // creo il list items concatenandolo alla variabile item
+    items += `<li>${numRandom[i]}</li>`
+}
+
+// assegno a numRandomList
+numRandomList.innerHTML = items
