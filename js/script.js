c026cfe745a14934909ff4afb2b7fba8c16eed41
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-27 15:36:32 +0100

    [UPD] Recupero elementi dal dom

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Recupero elementi dal DOM
+
+
+const countdown = document.getElementById('countdown')
+const numRandomList = document.getElementById('numbers-list')
+const numUser = document.querySelector('input')
+const button = document.querySelector('button')
+const message = document.getElementById('message')
+
+// Definisco l'array che conterrà i numeri random
+const numRandom = [''];
